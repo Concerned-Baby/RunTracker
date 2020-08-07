6841e7f8578286596c3a5913cfa2d288f3a98ce0
Spencer Ye <57966319+TheSteak-dev@users.noreply.github.com>
2020-08-07 15:33:06 -0700

    New Calculating Method

--- __test__/HSPOINTTEST.py
+++ __test__/HSPOINTTEST.py
@@ -4,24 +4,18 @@ def getPointsHSEvent(event, time):
 
 	EQUALCONSTANT = 150
 	#time = self.getPREvent(event)
-	if event == "100m":
+	if event == "100m": 
 		THRES = 11.59
 		pr = time
 		if pr < THRES:
-			return pow((THRES - pr + 3) / 1.1, 2) * 20.25
+			return 5.1 * pow((THRES - time + 7.2) / 1.1, 1.8)
 		elif (pr > THRES):
-			return pow((10 - pr + THRES) / 1.1, 2) * 1.8
-		return EQUALCONSTANT
-	elif event == "200m":
-		THRES = 23.8
-		pr = time
-		if pr < THRES:
-			return pow((THRES - pr + 3.3) / 1.2, 2) * 19.82
-		elif (pr > THRES):
-			return pow((11 - pr + THRES) / 1.2, 2) * 1.78
+			return 3 * pow((THRES - time + 14) / 1.1, 1.4) + 44
 		return EQUALCONSTANT
 	return -1
 
+#rewrite with a new function
+#graph on desmos
 
 values = [23, 23.5, 23.7, 23.79, 23.8, 23.81, 23.9, 25, 27, 30, 32]
 for value in values:
