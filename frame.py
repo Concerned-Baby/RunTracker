38e0f5bb687c97a1975b94a3ddff3c48c89421f3
Spencer Ye <57966319+TheSteak-dev@users.noreply.github.com>
2020-08-23 17:25:10 -0700

    Delete frame.py

--- frame.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-class Frame(tk.Frame):
-	def __init__ (self, window, name):
-		super().__init__(master=window, height=450, width=800, borderwidth=2, relief="groove")
-		self.name = name
-	def toString(self):
-		return self.name
