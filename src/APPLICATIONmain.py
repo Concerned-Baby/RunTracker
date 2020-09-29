8c94a43e0fee2819dd0d32c5216df0cb648430de
Spencer Ye <sye348@student.fuhsd.org>
2020-09-28 20:18:04 -0700

    ideas

--- src/APPLICATIONmain.py
+++ src/APPLICATIONmain.py
@@ -17,13 +17,13 @@ make goals screen bigger
 
 add units
 
-local rankings for event
+add uninstall
 
 update help menus
 
 clean up remove option (trashed)
 
-make sure pr works right with new formatting, (currently displays 16:40 as that was the old default value, need to change it)
+
 
 """
 
