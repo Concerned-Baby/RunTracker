822973d7ec804bdc01de2aeb46681736a3efaaaa
Spencer Ye <sye348@student.fuhsd.org>
2020-10-06 19:46:08 -0700

    update uninstall page

--- src/uninstall.py
+++ src/uninstall.py
@@ -14,15 +14,15 @@ def start():
 
 	frm_main = tk.Frame(master=window, height=200, width=500, borderwidth=2, relief="groove")
 	confirmBoxLabel = tk.Label(master=frm_main, text='Type "confirm" to Uninstall')
-	confirmBoxLabel.place(x=200, y=100)
+	confirmBoxLabel.place(x=175, y=50)
 	text = tk.StringVar()
 	entry = tk.Entry(master=frm_main, width=15, textvariable=text)
-	entry.place(x=200, y=120)
+	entry.place(x=200, y=70)
 	def check():
 		if (entry.get() == "confirm"):
 			window.destroy()
 			subprocess.call(str(pathlib.Path(__file__).parent.parent.absolute()) + "\\__del__.bat")
 	go = tk.Button(master=frm_main, text="Go", command=check, borderwidth=3, relief="raised", width=10, height=1)
-	go.place(x=200, y=150)
+	go.place(x=205, y=100)
 	frm_main.pack()
 	window.mainloop()
