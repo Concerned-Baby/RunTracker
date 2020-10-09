ba48abc2a78c255e5747f9301169dd4d421fbdf3
Spencer Ye <sye348@student.fuhsd.org>
2020-10-08 20:14:16 -0700

    update deletion

--- __del__.bat
+++ __del__.bat
@@ -1,4 +1,15 @@
 call :deleteSelf&exit /b
 :deleteSelf
 DEL "testdelete.txt"
+DEL ".git"
+DEL "css"
+DEL "HTML Pages"
+DEL "res"
+DEL "Runners"
+DEL "src"
+DEL ".gitignore"
+DEL "README.md"
+DEL "xApplication.bat"
+DEL "xGenerate.bat"
+DEL "xUninstall.bat"
 start /b "" cmd /c del "%~f0"&exit /b
